eafeaf225cf58b1c92ad2a58d6e6972d376c9a50
gaumeo01 <trandinhchienthang@gmail.com>
2025-06-15 13:07:37 +0700

    add solution for #15-3Sum

---
+++ Medium/15-3Sum/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int i, j, k;
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) 
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            j = i + 1;
+            k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                        k--;
+                    j++;
+                    k--;
+                }
+
+                else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
